0852130d96e30f3ffcc887d1b59533d851d6f7f0
hurui <782924665@qq.com>
2020-03-30 17:00:50 +0800

    collectionViewlayout系统方法重写完成

---
+++ SheetView_Demo/SheetView_Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SheetView_Demo
+//
+//  Created by lanyee-ios2 on 2020/3/27.
+//  Copyright © 2020 lanyee-ios2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SheetView_Demo/SheetView_DemoTests/SheetView_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  SheetView_DemoTests.swift
+//  SheetView_DemoTests
+//
+//  Created by lanyee-ios2 on 2020/3/27.
+//  Copyright © 2020 lanyee-ios2. All rights reserved.
+//
+
+import XCTest
+@testable import SheetView_Demo
+
+class SheetView_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
